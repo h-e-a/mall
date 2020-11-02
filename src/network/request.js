1ea4505f6228b0027ba2d1e60e1df02754ab7be5
李鸿杰 <hea1599@163.com>
2020-11-02 15:53:40 +0800

    commit

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config) {
   const  instance = axios.create({
-    baseURL:'',
+    baseURL:'接口关闭',
     timeout:5000,
     headers:{'Content-Type':'multipart/form-data'}
   })
